f20159e8ada54ecea86cdd4a9a1315159d9a42ba
Michael Bastos <bastosmichael@gmail.com>
2019-02-24 09:44:16 -0600

    initial commit

---
+++ assets_files/custom.js
@@ -0,0 +1,34 @@
+// Custom Scripts for Primal Template //
+
+jQuery(function($) {
+    "use strict";
+
+
+/* --------- Wow Init ------ */
+
+  new WOW().init();
+
+
+  /* ------ Countdown ----- */
+
+  $('#countdown').countdown({
+  			date: '12/01/2016 17:00:00',
+  			offset: +2,
+  			day: 'Day',
+  			days: 'Days'
+  		}, function () {
+  			alert('Done!');
+  		});
+
+
+/*----- Preloader ----- */
+
+    $(window).onload = function() {
+  		setTimeout(function() {
+        $('#loading').fadeOut('slow', function() {
+        });
+      }, 500);
+    };
+
+
+});
